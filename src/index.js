a4adbdbf96b2b0604fd7ec295f85e972e2547537
Dara Medeiros <daraamedeiros@outlook.com>
2020-03-17 10:06:28 -0300

    update lifting state up, again

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares
-      }]),
+      }]), // metodo concat() não modifica o array original
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -42,7 +42,7 @@ class Game extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-
+    
     return (
       <div className="game">
         <div className="game-board">
@@ -61,3 +61,24 @@ class Game extends React.Component {
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 serviceWorker.unregister();
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
