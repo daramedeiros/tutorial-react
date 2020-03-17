89b660d7f838571d8849d4e41f1e63d49bd6ee56
Dara Medeiros <daraamedeiros@outlook.com>
2020-03-16 21:14:56 -0300

    update tutorial

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,13 +2,6 @@ import React from "react"
 import Square from "./Square"
 
 class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares : Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
@@ -25,8 +18,8 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]} 
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]} 
+                onClick={() => this.props.Click(i)}
             />
         );
     }
